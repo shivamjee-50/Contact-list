a7c3408706bccb9525f637f5991250687c1108af
Shivam Jee <sjee@me.iitr.ac.in>
2021-07-22 23:02:16 +0530

    Added a few comments to the index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+//Require Express to be used in the project
 const express = require('express');
 const port = 8000;
 
+//Require mongoose to be used in the project from config folder
 const db = require('./config/mongoose');
+//Require Schema from the models
 const Contact = require('./models/contact');
 
 const app = express();
 
+//Setting the view engine and the views folder
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+//Setting up the router
 app.use('/', require('./routes'));
+
+//Using static files in the folder
 app.use(express.static('./assets'));
 
+//Firing up the server
 app.listen(port, function(err){
     if(err){
         console.log('Error in running the server', err);
