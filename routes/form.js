9eed2257e7a743d380c32f2b7cb554c34ccd3f7e
Shivam Jee <sjee@me.iitr.ac.in>
2021-07-22 15:26:43 +0530

    A few changes based on the post request

--- routes/form.js
+++ routes/form.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const form_controller = require('../controllers/form_controller');
 
 router.get('/',  form_controller.view);
-router.use('/submission', require('./form_submission'));
+router.post('/', form_controller.submit);
 
 
 module.exports = router;
